d1b55640deb4867151bdab09d21d9a2791a20d00
ben little <ben.little@benlittle.dev>
2024-07-20 16:55:33 -0600

    fix broken imports

--- tus/src/lib.rs
+++ tus/src/lib.rs
@@ -1,3 +1,5 @@
+use web_sys_resumable::ResumableUpload;
+
 /// Registers a new resumable upload with the remote located at `href`.
 /// Returns the resumable upload metadata.
 pub async fn new_upload<'a>(
@@ -6,8 +8,6 @@ pub async fn new_upload<'a>(
     chunk_sz: i32,
 ) -> anyhow::Result<(ResumableUpload<'a>, String)> {
     let upload = ResumableUpload::new(file, chunk_sz).await?;
-    log!("upload size: {}", file.size());
-    log!("number of chunks: {}", (file.size() as i32 / chunk_sz));
     let res = gloo_net::http::Request::post(href)
         .header("Content-Length", "0")
         .header("Upload-Length", upload.size().to_string().as_str())
@@ -33,11 +33,9 @@ pub async fn continue_upload<'a>(
     upload: &'a mut ResumableUpload<'a>,
     location: &str,
 ) -> anyhow::Result<()> {
-    let nchunks = upload.chunks();
     let chunk_sz = upload.chunk_size();
     upload
         .for_each_unsent(move |i, text| async move {
-            log!("uploading chunk {}/{}: {}", i + 1, nchunks, text);
             let res = gloo_net::http::Request::patch(location)
                 .header("Content-Length", text.len().to_string().as_str())
                 .header("Upload-Offset", (i * chunk_sz).to_string().as_str())
@@ -61,19 +59,19 @@ pub async fn continue_upload<'a>(
 #[cfg(test)]
 mod tests {
 
+    use super::*;
+    use wasm_bindgen::JsValue;
+    use wasm_bindgen_test::*;
+
     #[wasm_bindgen_test]
-    async fn main() {
+    async fn full_upload() {
         let file = {
             let str_seq = js_sys::Array::new();
             str_seq.push(&JsValue::from_str(&"blah".repeat(5)));
             web_sys::File::new_with_str_sequence(&str_seq, "blah.txt").unwrap()
         };
         let href = "http://localhost:1080/files/";
-        log!("creating new upload at {}", href);
         let (mut upload, location) = new_upload(&file, href, 3).await.unwrap();
-        log!("successfully created at {}", location);
-        log!("uploading content");
         continue_upload(&mut upload, &location).await.unwrap();
-        log!("content uploaded successfully!");
     }
 }
